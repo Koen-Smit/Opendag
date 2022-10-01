15080eb3fa867dfc9732b86dec97b9511a11ca08
Koen-Smit <93587490+Koen-Smit@users.noreply.github.com>
2022-10-01 15:26:56 +0200

    test
    test

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curio</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="algemeen.php">Algemeen</a></li>
+                <li><a href="vakken.php">Vakken</a></li>
+                <li><a href="rooster.php">Rooster</a></li>
+                <li><a href="blokken.php">Blokken</a></li>
+                <li><a href="versnelopties.php">Versnel mogelijkheden</a></li>
+                <li><a href="docent.php">Docenten</a></li>
+                <li><a href="foto.php">Foto's</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="banner"></div>
+        <div class="container">
+            <h1>aaaa</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero temporibus saepe, ad non ut laborum maxime unde veritatis assumenda. Mollitia corrupti tenetur quisquam non ducimus ratione accusamus suscipit neque. Doloremque.</p>
+        </div>
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
